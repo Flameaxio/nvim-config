36ed80c27ac094931cad81ad2772e09e74dfab10
Alexandr Voinalovych <olexandr.voinalovych@gotoinc.co>
2025-08-26 14:16:04 +0300

    feat: add harpoon plugin

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,11 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>ff', function()
+	require('telescope.builtin').find_files()
+end, { desc = 'Telescope find files' })
+
+vim.keymap.set('n', '<C-p>', function()
+	require('telescope.builtin').git_files()
+end, { desc = 'Telescope find files' })
+
 vim.keymap.set('n', '<leader>fs', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+	require('telescope.builtin').grep_string({ search = vim.fn.input("Grep > ") })
 end)
---
+++ lua/flameaxe/lazy.lua
@@ -0,0 +1,37 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  
+  'Mofiqul/dracula.nvim',
+  
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate'
+  },
+  {
+    'prochri/telescope-all-recent.nvim',
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "kkharji/sqlite.lua",
+      "stevearc/dressing.nvim"
+    },
+    opts = {}
+  },
+  'ThePrimeagen/harpoon'
+})
